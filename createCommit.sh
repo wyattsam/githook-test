b9497c91eccd489a626acee838479eb7616b0677
Sam Wyatt <sam.wyatt@10gen.com>
2015-11-23 14:51:16 -0500

    XGENTOOLS-1859 - added script to create commit

---
+++ createCommit.sh
@@ -0,0 +1,6 @@
+# a simple file to create a commit message that is then used to test the githook
+
+date > currentTime
+
+git add -a -m "XGENTOOLS-1859 - automatically-generated commit for testing"
+
